c1639bc6400ae46745f13d7f8d9b7c3faff6169b
mohammedshine <34446299+mohammedshine@users.noreply.github.com>
2020-06-07 20:29:38 +0530

    Create fifosearcher.py

---
+++ fifosearcher.py
@@ -0,0 +1,33 @@
+from urllib2 import Request, urlopen, URLError, HTTPError
+
+def Space(j):
+	i = 0
+	while i<=j:
+		print " ",
+		i+=1
+
+
+def findAdmin():
+	f = open("dicc.txt","r");
+	link = raw_input("Enter Site Name \n(without HTTP/HTTPS): ")
+	print "\n\nAvilable links : \n"
+	while True:
+		sub_link = f.readline()
+		if not sub_link:
+			break
+		req_link = "http://"+link+"/"+sub_link
+		req = Request(req_link)
+		try:
+			response = urlopen(req)
+		except HTTPError as e:
+			continue
+		except URLError as e:
+			continue
+		else:
+			print "OK => ",req_link
+
+def Credit():
+	print "TEST PROJECT BY SHINE"
+
+Credit()
+findAdmin()
